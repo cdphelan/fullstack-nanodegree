54e9f1d1144f802ab863d7488da649d3c22ac5f2
cdphelan <cdphelan@umich.edu>
2015-06-08 22:38:31 -0400

    Update media.py

--- media.py
+++ media.py
@@ -1,10 +1,9 @@
 import webbrowser
 
+'''This class provides a way to store movie-related information'''
 class Movie():
-    def __init__(self, movie_title, rt_consensus, rt_rating, poster_image, trailer_youtube):
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.consensus = rt_consensus
-        self.rating = rt_rating
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
